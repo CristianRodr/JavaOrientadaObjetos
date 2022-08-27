2c3189b9a6a16609f2b5874c634535597808424d
Cristian Rodriguez Quintero <an1me@hotmail.es>
2022-08-27 15:11:10 -0500

    creando objetos con el operador new

--- src/EjemploAutomovil.java
+++ src/EjemploAutomovil.java
@@ -1,5 +1,27 @@
 public class EjemploAutomovil {
     public static void main(String[] args) {
+        
+        Automovil auto = new Automovil();
 
+        auto.fabricante = "Subaru";
+        auto.modelo = "Impresa";
+        auto.cilidrada = 2.0;
+        auto.color = "Blanco";
+
+        Automovil mazda = new Automovil();
+        mazda.fabricante = "Mazda";
+        mazda.modelo = "BT-50";
+        mazda.cilidrada = 3.0;
+        mazda.color = "Rojo";
+
+        System.out.println("auto.fabricante = " + auto.fabricante);
+        System.out.println("auto.modelo = " + auto.modelo);
+        System.out.println("auto.color = " + auto.color);
+        System.out.println("auto.cilidrada = " + auto.cilidrada);
+
+        System.out.println("mazda.fabricante = " + mazda.fabricante);
+        System.out.println("mazda.modelo = " + mazda.modelo);
+        System.out.println("mazda.color = " + mazda.color);
+        System.out.println("mazda.cilidrada = " + mazda.cilidrada);
     }
 }
